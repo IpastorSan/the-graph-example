4daf95b9b7e90589fe4f9c1b79bf232a39785fd5
IpastorSan <ignaciopastorsan@gmail.com>
2023-09-12 20:01:16 +0200

    .

---
+++ smart-contract-subgraph/src/simple-marketplace.ts
@@ -0,0 +1,95 @@
+import {
+  ItemSold as ItemSoldEvent,
+  ListingCancelled as ListingCancelledEvent,
+  ListingCreated as ListingCreatedEvent,
+  ListingUpdated as ListingUpdatedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent
+} from "../generated/SimpleMarketplace/SimpleMarketplace"
+import {
+  ItemSold,
+  ListingCancelled,
+  ListingCreated,
+  ListingUpdated,
+  OwnershipTransferred
+} from "../generated/schema"
+
+export function handleItemSold(event: ItemSoldEvent): void {
+  let entity = new ItemSold(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.listingId = event.params.listingId
+  entity.nft = event.params.nft
+  entity.seller = event.params.seller
+  entity.buyer = event.params.buyer
+  entity.price = event.params.price
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleListingCancelled(event: ListingCancelledEvent): void {
+  let entity = new ListingCancelled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.listingId = event.params.listingId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleListingCreated(event: ListingCreatedEvent): void {
+  let entity = new ListingCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.listingId = event.params.listingId
+  entity.seller = event.params.seller
+  entity.nft = event.params.nft
+  entity.tokenId = event.params.tokenId
+  entity.price = event.params.price
+  entity.exist = event.params.exist
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleListingUpdated(event: ListingUpdatedEvent): void {
+  let entity = new ListingUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.listingId = event.params.listingId
+  entity.seller = event.params.seller
+  entity.nft = event.params.nft
+  entity.tokenId = event.params.tokenId
+  entity.price = event.params.price
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ smart-contract-subgraph/tests/simple-marketplace-utils.ts
@@ -0,0 +1,158 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts"
+import {
+  ItemSold,
+  ListingCancelled,
+  ListingCreated,
+  ListingUpdated,
+  OwnershipTransferred
+} from "../generated/SimpleMarketplace/SimpleMarketplace"
+
+export function createItemSoldEvent(
+  listingId: BigInt,
+  nft: Address,
+  seller: Address,
+  buyer: Address,
+  price: BigInt
+): ItemSold {
+  let itemSoldEvent = changetype<ItemSold>(newMockEvent())
+
+  itemSoldEvent.parameters = new Array()
+
+  itemSoldEvent.parameters.push(
+    new ethereum.EventParam(
+      "listingId",
+      ethereum.Value.fromUnsignedBigInt(listingId)
+    )
+  )
+  itemSoldEvent.parameters.push(
+    new ethereum.EventParam("nft", ethereum.Value.fromAddress(nft))
+  )
+  itemSoldEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemSoldEvent.parameters.push(
+    new ethereum.EventParam("buyer", ethereum.Value.fromAddress(buyer))
+  )
+  itemSoldEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return itemSoldEvent
+}
+
+export function createListingCancelledEvent(
+  listingId: BigInt
+): ListingCancelled {
+  let listingCancelledEvent = changetype<ListingCancelled>(newMockEvent())
+
+  listingCancelledEvent.parameters = new Array()
+
+  listingCancelledEvent.parameters.push(
+    new ethereum.EventParam(
+      "listingId",
+      ethereum.Value.fromUnsignedBigInt(listingId)
+    )
+  )
+
+  return listingCancelledEvent
+}
+
+export function createListingCreatedEvent(
+  listingId: BigInt,
+  seller: Address,
+  nft: Address,
+  tokenId: BigInt,
+  price: BigInt,
+  exist: boolean
+): ListingCreated {
+  let listingCreatedEvent = changetype<ListingCreated>(newMockEvent())
+
+  listingCreatedEvent.parameters = new Array()
+
+  listingCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "listingId",
+      ethereum.Value.fromUnsignedBigInt(listingId)
+    )
+  )
+  listingCreatedEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  listingCreatedEvent.parameters.push(
+    new ethereum.EventParam("nft", ethereum.Value.fromAddress(nft))
+  )
+  listingCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  listingCreatedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+  listingCreatedEvent.parameters.push(
+    new ethereum.EventParam("exist", ethereum.Value.fromBoolean(exist))
+  )
+
+  return listingCreatedEvent
+}
+
+export function createListingUpdatedEvent(
+  listingId: BigInt,
+  seller: Address,
+  nft: Address,
+  tokenId: BigInt,
+  price: BigInt
+): ListingUpdated {
+  let listingUpdatedEvent = changetype<ListingUpdated>(newMockEvent())
+
+  listingUpdatedEvent.parameters = new Array()
+
+  listingUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "listingId",
+      ethereum.Value.fromUnsignedBigInt(listingId)
+    )
+  )
+  listingUpdatedEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  listingUpdatedEvent.parameters.push(
+    new ethereum.EventParam("nft", ethereum.Value.fromAddress(nft))
+  )
+  listingUpdatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  listingUpdatedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return listingUpdatedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
